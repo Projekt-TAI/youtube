4fd13519e15aaebb41b3b5f8a546043b67ca8b0f
muter3000 <38788725+muter3000@users.noreply.github.com>
2023-11-20 17:19:57 +0100

    Init (#9)
    * Initial api
    
    * Test controller
    
    ---------
    
    Co-authored-by: Chwila <bartosz.chwila@sap.com>

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.OpenApi.Models;
+using TAIBackend.routes.oauth;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+     c.SwaggerDoc("v1", new OpenApiInfo { Title = "TAI API", Description = "TAI Project video player api", Version = "v1.0.0" });
+});
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+   c.SwaggerEndpoint("/swagger/v1/swagger.json", "TAI API V1");
+});
+
+
+app.MapControllers();
+
+app.Run();
