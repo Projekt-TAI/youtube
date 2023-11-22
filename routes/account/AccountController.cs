fad74d77d5ed83c32967ffd6482b330560f2b009
muter3000 <muter3000@gmail.com>
2023-11-22 19:25:05 +0100

    Maybe working with front

---
+++ routes/account/AccountController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Facebook;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TAIBackend.routes.account;
+
+[Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme), Route("account")]
+public class AccountController : Controller
+{
+    [AllowAnonymous]
+    [HttpGet("facebook-login")]
+    public IActionResult FacebookLogin()
+    {
+        var properties = new AuthenticationProperties { RedirectUri = "https://locahost:3000" };
+        return Challenge(properties, FacebookDefaults.AuthenticationScheme);
+    }
+
+    [HttpGet("details")]
+    public IActionResult Details()
+    {
+        return Json("a");
+    }
+    
+}
