804df79e3538449318274e9754744cae1732d91b
muter3000 <38788725+muter3000@users.noreply.github.com>
2023-11-22 15:08:12 +0100

    Update login to not use my own bad OIDC flow (#15)
    * Remove unneeded
    
    * update
    
    * update
    
    * OIDC flow with facebook
    
    * Update routes
    
    * in progress
    
    * Dont use my own bad implementation
    
    * Tidy

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Facebook;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,19 @@ builder.Services.AddSwaggerGen(c =>
      c.SwaggerDoc("v1", new OpenApiInfo { Title = "TAI API", Description = "TAI Project video player api", Version = "v1.0.0" });
 });
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = FacebookDefaults.AuthenticationScheme;
+}).AddFacebook(options =>
+{
+    options.AppId = builder.Configuration["Authentication:Facebook:AppId"] ?? throw new InvalidOperationException();
+    options.AppSecret = builder.Configuration["Authentication:Facebook:AppSecret"] ??
+                        throw new InvalidOperationException();
+    options.CorrelationCookie.Path = "/"; 
+}).AddCookie();
+
+builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 
 var app = builder.Build();
@@ -18,6 +33,13 @@ app.UseSwaggerUI(c =>
    c.SwaggerEndpoint("/swagger/v1/swagger.json", "TAI API V1");
 });
 
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/error");
+}
+
+app.UseAuthorization();
+app.UseAuthentication();
 app.MapControllers();
 
 app.Run();
