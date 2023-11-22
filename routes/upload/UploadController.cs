5051c38c5cb62f4f1e262dd8cdb5662921e56958
muter3000 <muter3000@gmail.com>
2023-11-22 12:36:53 +0100

    Dont use my own bad implementation

--- routes/upload/UploadController.cs
+++ routes/upload/UploadController.cs
@@ -1,15 +1,19 @@
+using System.Net;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.Facebook;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol;
 
 namespace TAIBackend.routes.upload;
 
-[Authorize]
+[Authorize(AuthenticationSchemes = FacebookDefaults.AuthenticationScheme)]
 public class UploadController : Controller
 {
     [HttpGet("upload")]
     public IActionResult Upload()
     {
-        return Ok();
+        var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        return Ok(userId);
     }
 }
