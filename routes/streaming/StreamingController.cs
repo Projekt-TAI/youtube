7e825cb1e8f00fcb164cc10a4dd695b804fd531b
muter3000 <38788725+muter3000@users.noreply.github.com>
2023-12-01 15:48:34 +0100

    Upload update (#22)
    * File upload controller
    
    * File upload utilities
    
    * Dash in progress
    
    * Working file upload and streaming
    
    * Delete a
    
    * Add configuration for running in docker
    
    * Update test.html
    
    * Update dotnet-docker-build.yaml
    
    * Update dotnet-docker-build.yaml
    
    * Add readyz
    
    * Enforce https
    
    * Use https forward
    
    * File upload (#2)
    
    * GHA in progress
    
    * Continue with charts
    
    * docker
    
    * Update fragment
    
    * Update README.md
    
    * Add python to image
    
    * Add python to image
    
    * Same-site=none
    
    * Change upload api
    
    * Check if title exist
    
    ---------
    
    Co-authored-by: Chwila <bartosz.chwila@sap.com>

--- routes/streaming/StreamingController.cs
+++ routes/streaming/StreamingController.cs
@@ -5,28 +5,17 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.WebUtilities;
 using TAIBackend.Utilities;
 using Microsoft.AspNetCore.Http.Features;
-using System.Text;
 using System.Security.Claims;
-using Newtonsoft.Json;
 
 namespace TAIBackend.routes.streaming;
 
 [Route("videos")]
 public class StreamingController : Controller
 {
-    private class VideoDescriptor
-    {
-        [JsonProperty("id")]
-        public int? Id;
-        [JsonProperty("title")]
-        public string? Title;
-    }
-
-
     private readonly long _fileSizeLimit;
     private readonly ILogger<StreamingController> _logger;
     private readonly string[] _permittedExtensions = { ".mp4" };
-    private readonly string? _targetFilePath;
+    private readonly string _targetFilePath;
 
     private readonly string _mp4DashPath;
     private readonly string _mp4FragmentPath;
@@ -38,15 +27,15 @@ public class StreamingController : Controller
     {
         _logger = logger;
         _fileSizeLimit = config.GetValue<long>("FileSizeLimit");
-        _targetFilePath = config.GetValue<string>("StoredFilesPath");
-        _mp4DashPath = config.GetValue<string>("MP4DashPath");
-        _mp4FragmentPath = config.GetValue<string>("MP4FragmentPath");
+        _targetFilePath = config.GetValue<string>("StoredFilesPath") ?? throw new InvalidOperationException();;
+        _mp4DashPath = config.GetValue<string>("MP4DashPath") ?? throw new InvalidOperationException();;
+        _mp4FragmentPath = config.GetValue<string>("MP4FragmentPath") ?? throw new InvalidOperationException();;
     }
 
     [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
-    [HttpPost("upload"), HttpPut("upload")]
+    [HttpPost("upload/{id}"), HttpPut("upload/{id}")]
     [DisableFormValueModelBinding]
-    public async Task<IActionResult> UploadPhysical()
+    public async Task<IActionResult> UploadPhysical(uint id)
     {
         if (Request.ContentType == null)
         {
@@ -66,13 +55,13 @@ public class StreamingController : Controller
             MediaTypeHeaderValue.Parse(Request.ContentType),
             _defaultFormOptions.MultipartBoundaryLengthLimit);
         var reader = new MultipartReader(boundary, HttpContext.Request.Body);
-        var section = await reader.ReadNextSectionAsync();
 
         if (User.FindFirst(ClaimTypes.NameIdentifier) == null)
         {
             return StatusCode(500);
         }
-        var userDirectory = Base64UrlTextEncoder.Encode(Encoding.ASCII.GetBytes(User.FindFirst(ClaimTypes.NameIdentifier)!.Value));
+
+        var userDirectory = User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
         if (_targetFilePath == null || userDirectory == null)
         {
             return StatusCode(500);
@@ -81,28 +70,7 @@ public class StreamingController : Controller
         var userPath = Path.Combine(_targetFilePath, userDirectory);
         Directory.CreateDirectory(userPath);
 
-        if (section == null || section!.ContentType != "application/json")
-        {
-            return BadRequest();
-        }
-
-        var json = await section.ReadAsStringAsync();
-        if (json == null)
-        {
-            return BadRequest();
-        }
-        VideoDescriptor? videoDescriptor = JsonConvert.DeserializeObject<VideoDescriptor>(json);
-        if (videoDescriptor == null)
-        {
-            return BadRequest();
-        }
-        if (videoDescriptor.Id == null)
-        {
-            return BadRequest("Video id is required");
-        }
-
-
-        var videoDirectory = Path.Combine(userPath, $"{videoDescriptor.Id}");
+        var videoDirectory = Path.Combine(userPath, $"{id}");
         Directory.CreateDirectory(videoDirectory);
 
         var trustedFileNameForFileStorage = "video";
@@ -114,83 +82,108 @@ public class StreamingController : Controller
             }
         }
 
-        using (var targetStream = System.IO.File.Create(
-            Path.Combine(videoDirectory, $"{trustedFileNameForFileStorage}.json")))
+        var section = await reader.ReadNextSectionAsync();
+        var title = "";
+        var description = "";
+        bool gotFile = false;
+
+        while (section != null)
         {
-            await targetStream.WriteAsync(Encoding.ASCII.GetBytes(json));
+            var hasContentDispositionHeader =
+                ContentDispositionHeaderValue.TryParse(
+                    section.ContentDisposition, out var contentDisposition);
 
-            _logger.LogInformation(
-                "Uploaded file config, saved to " +
-                "'{VideoPath}' as {TrustedFileNameForFileStorage}.json",
-                videoDirectory,
-                trustedFileNameForFileStorage);
-        }
-
-        section = await reader.ReadNextSectionAsync();
-        if (section == null)
-        {
-            return BadRequest();
-        }
-        var hasContentDispositionHeader =
-            ContentDispositionHeaderValue.TryParse(
-                section.ContentDisposition, out var contentDisposition);
-
-        if (!hasContentDispositionHeader)
-        {
-            return BadRequest();
-        }
-
-        // This check assumes that there's a file
-        // present without form data. If form data
-        // is present, this method immediately fails
-        // and returns the model error.
-        if (!MultipartRequestHelper
-            .HasFileContentDisposition(contentDisposition!))
-        {
-            ModelState.AddModelError("File",
-                $"The request couldn't be processed (Error 2).");
-            // Log error
-
-            return BadRequest(ModelState);
-        }
-        else
-        {
-
-            // **WARNING!**
-            // In the following example, the file is saved without
-            // scanning the file's contents. In most production
-            // scenarios, an anti-virus/anti-malware scanner API
-            // is used on the file before making the file available
-            // for download or for use by other systems. 
-            // For more information, see the topic that accompanies 
-            // this sample.
-
-            var streamedFileContent = await FileHelpers.ProcessStreamedFile(
-                section, contentDisposition!, ModelState,
-                _permittedExtensions, _fileSizeLimit);
-
-            if (!ModelState.IsValid)
+            if (!hasContentDispositionHeader)
             {
+                return BadRequest();
+            }
+
+            if (MultipartRequestHelper.HasFormDataTextContentDisposition(contentDisposition!))
+            {
+                var formData = section.AsFormDataSection();
+                if (formData == null)
+                {
+                    return BadRequest();
+                }
+
+                switch (formData.Name.ToLower())
+                {
+                    case "title":
+                        title = await formData.GetValueAsync();
+                        Console.WriteLine(title);
+                        break;
+                    case "description":
+                        description = await formData.GetValueAsync();
+                        break;
+                }
+
+                section = await reader.ReadNextSectionAsync();
+                continue;
+            }
+
+            if (gotFile)
+            {
+                return BadRequest("Only one video upload per request is possible");
+            }
+
+            gotFile = true;
+
+            if (!MultipartRequestHelper
+                .HasFileContentDisposition(contentDisposition!))
+            {
+                ModelState.AddModelError("File",
+                    $"The request couldn't be processed (Error 2).");
+                // Log error
+
                 return BadRequest(ModelState);
             }
-
-            using (var targetStream = System.IO.File.Create(
-                Path.Combine(videoDirectory, $"{trustedFileNameForFileStorage}.mp4")))
+            else
             {
-                await targetStream.WriteAsync(streamedFileContent);
 
-                _logger.LogInformation(
-                    "Uploaded file saved to " +
-                    "'{VideoPath}' as {TrustedFileNameForFileStorage}.mp4",
-                    videoDirectory,
-                    trustedFileNameForFileStorage);
+                // **WARNING!**
+                // In the following example, the file is saved without
+                // scanning the file's contents. In most production
+                // scenarios, an anti-virus/anti-malware scanner API
+                // is used on the file before making the file available
+                // for download or for use by other systems. 
+                // For more information, see the topic that accompanies 
+                // this sample.
+
+                var streamedFileContent = await FileHelpers.ProcessStreamedFile(
+                    section, contentDisposition!, ModelState,
+                    _permittedExtensions, _fileSizeLimit);
+
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                using (var targetStream = System.IO.File.Create(
+                    Path.Combine(videoDirectory, $"{trustedFileNameForFileStorage}.mp4")))
+                {
+                    await targetStream.WriteAsync(streamedFileContent);
+
+                    _logger.LogInformation(
+                        "Uploaded file saved to " +
+                        "'{VideoPath}' as {TrustedFileNameForFileStorage}.mp4",
+                        videoDirectory,
+                        trustedFileNameForFileStorage);
+                }
             }
+
+            if (MpegHelpers.GenerateDash(_mp4DashPath, _mp4FragmentPath, videoDirectory) != 0)
+            {
+                return StatusCode(500);
+            }
+
+            section = await reader.ReadNextSectionAsync();
         }
 
-        if (MpegHelpers.GenerateDash(_mp4DashPath, _mp4FragmentPath, videoDirectory) != 0)
+        if (!gotFile || String.IsNullOrWhiteSpace(title))
         {
-            return StatusCode(500);
+            return BadRequest();
         }
+        // TODO: Add title, desc and ID to DB
 
         return Created(nameof(StreamingController), null);
     }
@@ -199,13 +192,16 @@ public class StreamingController : Controller
     [HttpGet("{authorID}/{videoID}/manifest.mpd")]
     public IActionResult GetVideoManifest(string authorID, string videoID)
     {
-        var userDirectory = Base64UrlTextEncoder.Encode(Encoding.ASCII.GetBytes(authorID));
-        if (_targetFilePath == null || userDirectory == null)
+        if (_targetFilePath == null)
         {
             return StatusCode(500);
         }
 
-        var videoDirectory = Path.Combine(_targetFilePath, userDirectory, videoID);
+        var videoDirectory = Path.Combine(_targetFilePath, authorID, videoID);
+        if (!Path.Exists(Path.Combine(videoDirectory, "stream.mpd")))
+        {
+            return NotFound("DASH manifest doesn't exists");
+        }
 
         return PhysicalFile(Path.Combine(videoDirectory, "stream.mpd"), "application/xml");
     }
@@ -214,13 +210,16 @@ public class StreamingController : Controller
     [HttpGet("{authorID}/{videoID}/audio/{p1}/{p2}/{segmentNumber}")]
     public IActionResult GetAudioSegment(string authorID, string videoID, string p1, string p2, string segmentNumber)
     {
-        var userDirectory = Base64UrlTextEncoder.Encode(Encoding.ASCII.GetBytes(authorID));
-        if (_targetFilePath == null || userDirectory == null)
+        if (_targetFilePath == null)
         {
             return StatusCode(500);
         }
 
-        var videoDirectory = Path.Combine(_targetFilePath, userDirectory, videoID);
+        var videoDirectory = Path.Combine(_targetFilePath, authorID, videoID);
+        if (!Path.Exists(Path.Combine(videoDirectory, "audio", p1, p2, segmentNumber)))
+        {
+            return NotFound("Audio segment doesn't exists");
+        }
         return PhysicalFile(Path.Combine(videoDirectory, "audio", p1, p2, segmentNumber), "audio/aac");
     }
 
@@ -228,13 +227,16 @@ public class StreamingController : Controller
     [HttpGet("{authorID}/{videoID}/video/{p1}/{segmentNumber}")]
     public IActionResult GetVideoSegment(string authorID, string videoID, string p1, string segmentNumber)
     {
-        var userDirectory = Base64UrlTextEncoder.Encode(Encoding.ASCII.GetBytes(authorID));
-        if (_targetFilePath == null || userDirectory == null)
+        if (_targetFilePath == null)
         {
             return StatusCode(500);
         }
 
-        var videoDirectory = Path.Combine(_targetFilePath, userDirectory, videoID);
-        return PhysicalFile(Path.Combine(videoDirectory,"video", p1, segmentNumber), "video/mp4");
+        var videoDirectory = Path.Combine(_targetFilePath, authorID, videoID);
+        if (!Path.Exists(Path.Combine(videoDirectory, "video", p1, segmentNumber)))
+        {
+            return NotFound("Video segment doesn't exists");
+        }
+        return PhysicalFile(Path.Combine(videoDirectory, "video", p1, segmentNumber), "video/mp4");
     }
 }
