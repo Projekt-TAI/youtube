94318f614ffa19e0a55535133f9b4d2711c57fe7
tomabar188 <56341797+tomabar188@users.noreply.github.com>
2023-12-01 10:53:02 +0100

    Update Entities/User.cs
    Co-authored-by: Bartosz Chwila <103247439+barchw@users.noreply.github.com>

--- Entities/User.cs
+++ Entities/User.cs
@@ -5,8 +5,8 @@ namespace TAIBackend.Entities
      
         public int? Id { get; set; }
         public string? FirstName { get; set; } 
-        public string? LastName { get; set; }
-        public string? NickName { get; set; } 
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
          
 
 
